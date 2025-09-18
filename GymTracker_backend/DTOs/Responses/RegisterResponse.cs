573500aec87634f74a9846b3b73787954c65d7ff
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2025-09-18 19:55:40 +0200

    register response returns jwt now

--- GymTracker_backend/DTOs/Responses/RegisterResponse.cs
+++ GymTracker_backend/DTOs/Responses/RegisterResponse.cs
@@ -2,9 +2,6 @@ namespace GymTracker_backend.DTOs.Responses;
 
 public class RegisterResponse
 {
-    public Guid Id { get; set; }
-    public string Email { get; set; } = null!;
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
+    public string jwt { get; set; } = null!;
     
 }
